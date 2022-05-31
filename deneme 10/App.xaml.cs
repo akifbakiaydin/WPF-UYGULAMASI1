d7e2d17147c730c8162a1687561c4368a1f702d5
akifbakiaydin <akifbakiaydinnn@gmail.com>
2022-05-31 21:57:05 +0300

    WPF ÖDEV UYGULAMASI

---
+++ deneme 10/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace numara_kayıt_programı_1
+{
+     
+    public partial class App : Application
+    {
+    }
+}
---
+++ deneme 10/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace deneme_10
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void lst_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lst.Items.Add(new { ADI = txt1.Text, SOYADI = txt3.Text, NUMARASI = (Convert.ToInt32(txt2.Text)) });
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            lst.Items.Remove(lst.SelectedItem);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            lst.Items.Remove(lst.SelectedItem);
+        }
+    }
+}
+
